aaf10cbf332e297c07bbf4970f1b44ff25552140
baohan <65202910+baohodn93@users.noreply.github.com>
2022-03-18 22:41:24 +0900

    Upload and Download With Spring Boot REST API

---
+++ src/test/java/com/blogjava/FileUploadDownloadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadDownloadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
